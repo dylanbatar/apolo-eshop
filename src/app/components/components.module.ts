7ed96fae0cd80c4ebe03370b3a252907ca1e828c
dylanbatar <dylanbatar2@gmail.com>
2020-04-29 00:20:17 -0500

    Vista principal

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,24 @@ import { CommonModule } from "@angular/common";
 import { BannerComponent } from "./banner/banner.component";
 import { HorizontalScrollComponent } from "./horizontal-scroll/horizontal-scroll.component";
 import { CardComponent } from "./card/card.component";
+import { FeatureProductsComponent } from "./feature-products/feature-products.component";
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
-  declarations: [BannerComponent, HorizontalScrollComponent, CardComponent],
+  declarations: [
+    BannerComponent,
+    HorizontalScrollComponent,
+    CardComponent,
+    FeatureProductsComponent,
+    TabsComponent,
+  ],
   imports: [CommonModule],
-  exports: [BannerComponent, HorizontalScrollComponent, CardComponent],
+  exports: [
+    BannerComponent,
+    HorizontalScrollComponent,
+    CardComponent,
+    FeatureProductsComponent,
+    TabsComponent
+  ],
 })
 export class ComponentsModule {}
---
+++ src/app/components/feature-products/feature-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-products',
+  templateUrl: './feature-products.component.html',
+  styleUrls: ['./feature-products.component.scss']
+})
+export class FeatureProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
